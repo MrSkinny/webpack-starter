d0748310f37c8703b59f68b746b590c69de3d7a5
Rich Greenhill <rgnet1@yahoo.com>
2016-04-13 18:35:01 -0700

    use sass instead of less

--- js/background-changer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const $ = require('jquery');
-
-function makeBackgroundRed(){
-  $('body').css({ 'background-color': 'red' });
-}
-
-module.exports = makeBackgroundRed;
--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
-'use strict';
-
-const $ = require('jquery');
-const makeBackgroundRed = require('./background-changer.js');
-
-$(document).ready(function(){
-  console.log('hi from the index');
-  makeBackgroundRed();
-});
-
+console.log('hello');
